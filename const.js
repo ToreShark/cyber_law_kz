e759ecbf36b4f583c8406ffddaedf575e921b314
torekhan mukhtarov <nahkerot@gmail.com>
2022-09-26 16:13:40 +0600

    test cyber_bot

---
+++ const.js
@@ -0,0 +1,18 @@
+const command = `
+/start - Перезапустить бота
+/help - Помощь
+`
+
+const text1 = `
+test1 <a href = "https://www.youtube.com/watch?v=DPoLbZNmot0">Send</a>
+`
+const text2 = `
+test <a href = "https://www.youtube.com/watch?v=DPoLbZNmot0">Send</a>
+`
+const text3 = `
+test 3 <a href = "https://www.youtube.com/watch?v=DPoLbZNmot0">Send</a>
+`
+module.exports.command = command
+module.exports.text1 = text1
+module.exports.text2 = text2
+module.exports.text3 = text3
